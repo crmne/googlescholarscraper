7fd2fbb79408f162f9bc22d0d4a3b2bd8ef8a1de
Carmine Paolino <carmine@paolino.me>
2017-10-17 09:00:07 +0200

    Get article's title

--- googlescholar/spiders/googlescholar_spider.py
+++ googlescholar/spiders/googlescholar_spider.py
@@ -8,6 +8,17 @@ class GooglescholarSpider(scrapy.Spider):
         'https://scholar.google.de/scholar?q=((%22deep+learning%22+OR+%22deep+neural+network%22+OR+%22state-of-the-art%22)+AND+(%22genre+recognition%22+OR+%22genre+classification%22))&hl=en&as_sdt=0,5'
     ]
 
+    @staticmethod
+    def parse_title(article):
+        title_parts = [
+            i for i in article.xpath('h3//text()').extract()
+            if i.strip() != '[PDF]'
+        ]
+        return "".join(title_parts).strip()
+
     def parse(self, response):
-        with open('test.html', 'wb') as f:
-            f.write(response.body)
+        articles = response.xpath('//div[contains(@class, gs_ri)]/*[h3]')
+        for article in articles:
+            title = self.parse_title(article)
+            import ipdb
+            ipdb.set_trace()
